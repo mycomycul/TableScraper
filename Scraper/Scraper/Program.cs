35374c212eed66029bf559f7b57615e6904edde1
mycomycul <32719514+mycomycul@users.noreply.github.com>
2018-07-31 09:23:33 -0700

    Add Rainier NPS Trail Conditions

--- Scraper/Scraper/Program.cs
+++ Scraper/Scraper/Program.cs
@@ -16,15 +16,50 @@ namespace Scraper
     {
         static void Main(string[] args)
         {
+            var RainierConditions = RainierTrailConditions();
+            var OlympicConditions = OlympicTrailConditions();
+            Console.ReadLine();
+        }
+        ///<summary>
+        ///Removes comments and newlines from strings and replaces character codes
+        /// </summary>
+        static string CleanFromHTML(string stringToClean)
+        {
+            Regex HTMLCommentRegEx = new Regex("<[^>]*>", RegexOptions.IgnoreCase);
+            stringToClean = HTMLCommentRegEx.Replace(HttpUtility.HtmlDecode(stringToClean).Replace("\n", String.Empty), "");
+            return stringToClean;
+        }
+        //Need to account for oddities in elevations or mileagelike multiple mileage sets
+        static public float? ExtractMiles(string milesElevation)
+        {
+            try
+            {
+                milesElevation = milesElevation.Remove(milesElevation.IndexOf(" miles"));
+                return float.Parse(milesElevation);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+        //Need to account for oddities in elevations or mileagelike multiple mileage sets
+        static public string ExtractElevations(string milesElevation)
+        {
+            milesElevation = milesElevation.Remove(0, milesElevation.IndexOf("miles") + 5);
+            return milesElevation;
+        }
+
+        static List<TrailConditions> OlympicTrailConditions()
+        {
             List<TrailConditions> ConditionsList = new List<TrailConditions>();
             //Setup Browser and download page 
-            ScrapingBrowser Browser = new ScrapingBrowser();
-            Browser.AllowAutoRedirect = true;
-            Browser.AllowMetaRedirect = true;
-            WebPage PageResult = Browser.NavigateToPage(new Uri("https://www.nps.gov/olym/planyourvisit/wilderness-trail-conditions.htm"));
+            var baseUrl = new Uri("https://www.nps.gov/olym/planyourvisit/wilderness-trail-conditions.htm");
+            HtmlWeb web = new HtmlWeb();
+            var PageResult = web.Load(baseUrl.ToString());
+
 
             //Get tables from page
-            IEnumerable<HtmlNode> TableNode = PageResult.Html.CssSelect("tbody");
+            IEnumerable<HtmlNode> TableNode = PageResult.DocumentNode.CssSelect("tbody");
 
             //Loop through tables skipping table one
             for (int i = 1; i < TableNode.Count(); i++)
@@ -42,53 +77,52 @@ namespace Scraper
                         Elevation = ExtractElevations(CleanFromHTML(rowCells.ElementAt(2).InnerText)),
                         Conditions = CleanFromHTML(rowCells.ElementAt(3).InnerText),
                         Updated = DateTime.Parse(CleanFromHTML(rowCells.ElementAt(4).InnerText)),
-                        InfoLink = rowCells.ElementAt(0).CssSelect("a").Any() ? PageResult.AbsoluteUrl.Host + CleanFromHTML(rowCells.ElementAt(0).CssSelect("a").First().GetAttributeValue("href")) : null,
+                        InfoHTMLLink = rowCells.ElementAt(0).CssSelect("a").Any() ? baseUrl.Host + CleanFromHTML(rowCells.ElementAt(0).CssSelect("a").First().GetAttributeValue("href")) : null,
                         Zone = CleanFromHTML(rowZone),
                         Miles = ExtractMiles(CleanFromHTML(rowCells.ElementAt(2).InnerText))
 
                     };
-
-                    Console.WriteLine(tc.TrailName);
-                    Console.WriteLine(tc.Description);
                     ConditionsList.Add(tc);
                 }
-
             }
+            return ConditionsList;
+        }
 
+        static List<TrailConditions> RainierTrailConditions()
+        {
+            List<TrailConditions> ConditionsList = new List<TrailConditions>();
+            //Setup Browser and download page 
+            var baseUrl = new Uri("https://www.nps.gov/mora/planyourvisit/trails-and-backcountry-camp-conditions.htm");
+            HtmlWeb web = new HtmlWeb();
+            var PageResult = web.Load(baseUrl.ToString());
+            //Get tables from page
+            IEnumerable<HtmlNode> TableNode = PageResult.DocumentNode.CssSelect("tbody");
 
-            Console.ReadLine();
-
-            ///<summary>
-            ///Removes comments and newlines from strings and replaces character codes
-            /// </summary>
-            string CleanFromHTML(string stringToClean)
+            //Loop through tables skipping table one
+            for (int i = 0; i < TableNode.Count(); i++)
             {
-                Regex HTMLCommentRegEx = new Regex("<[^>]*>", RegexOptions.IgnoreCase);
-                stringToClean = HTMLCommentRegEx.Replace(HttpUtility.HtmlDecode(stringToClean).Replace("\n", String.Empty), "");
-                return stringToClean;
-            }
-            //Need to account for oddities in elevations or mileagelike multiple mileage sets
-            float? ExtractMiles(string milesElevation)
-            {
-                try
+                var tableRows = TableNode.ElementAt(i).CssSelect("tr");
+                //Loop Through Rows adding to database skipping rows one and 2
+                for (int j = 2; j < tableRows.Count(); j++)
                 {
-                    milesElevation = milesElevation.Remove(milesElevation.IndexOf(" miles"));
-                    return float.Parse(milesElevation);
-                }
-                catch
-                {
-                    return null;
-                }
-            }
-            //Need to account for oddities in elevations or mileagelike multiple mileage sets
-            string ExtractElevations(string milesElevation)
-            {
-                milesElevation = milesElevation.Remove(0, milesElevation.IndexOf("miles") + 5);
-                return milesElevation;
-            }
+                    var rowCells = tableRows.ElementAt(j).CssSelect("td");
+                    var rowZone = tableRows.ElementAt(0).InnerText;
 
+                    TrailConditions tc = new TrailConditions()
+                    {
+                        TrailName = CleanFromHTML(rowCells.ElementAt(0).InnerText),
+                        PercentSnowCover = int.TryParse(CleanFromHTML(rowCells.ElementAt(1).InnerText), out int snow) ? snow : 0,
+                        Conditions = CleanFromHTML(rowCells.ElementAt(2).InnerText),
+                        Updated = DateTime.TryParse(CleanFromHTML(rowCells.ElementAt(3).InnerText), out DateTime update) ? update : (DateTime?)null
+                        //Zone = CleanFromHTML(rowZone),
+                    };
+                    ConditionsList.Add(tc);
+                }          
+            }
+            return ConditionsList;
 
         }
+
 
 
         public class TrailConditions
@@ -102,9 +136,11 @@ namespace Scraper
             /// <summary>
             /// Link to information page on NPS.gov
             /// </summary>
-            public string InfoLink { get; set; }
-            public DateTime Updated { get; set; }
+            public string InfoHTMLLink { get; set; }
+            public int PercentSnowCover { get; set; }
+            public DateTime? Updated { get; set; }
 
         }
     }
 }
+
